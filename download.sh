0348e267883c69838be074a46d4e35ec2b5930ac
beknazar <beknazar23@gmail.com>
2019-03-26 15:19:11 +0900

    Add pyworld installation script

--- download.sh
+++ download.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+git clone https://github.com/JeremyCCHsu/Python-Wrapper-for-World-Vocoder.git
+cd Python-Wrapper-for-World-Vocoder
+git submodule update --init
+pip install -U pip
+pip install -r requirements.txt
+pip install .
+
 python download.py --download_dir ./download --data_dir ./data --datasets vcc2016
